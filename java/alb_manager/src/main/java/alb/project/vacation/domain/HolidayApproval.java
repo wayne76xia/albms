369f74496c434c031f9b545b42753dfb80433114
bearcat926 <43287323+bearcat926@users.noreply.github.com>
2021-11-23 20:12:12 +0800

    Create HolidayApproval.java

---
+++ java/alb_manager/src/main/java/alb/project/vacation/domain/HolidayApproval.java
@@ -0,0 +1,40 @@
+package alb.project.vacation.domain;
+
+import alb.framework.web.domain.BaseEntity;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Builder
+public class HolidayApproval extends BaseEntity
+{
+    private static final long serialVersionUID = 1L;
+
+    /* 假期审批ID */
+    private Long holidayApprovalId;
+
+    /* 假期类型 */
+    private Long holidayTypeId;
+
+    private String typeName;
+
+    private String typeValue;
+
+    /* 待审批人角色ID */
+    private Long roleId;
+
+    /* 审批人角色ID */
+    private Long approvedRoleId;
+
+    /* 当前审批序号 从1开始计数 */
+    private Integer currentApprovedIndex;
+
+    /* 下一级审批的的ID，若为最后一级审批则为0 */
+    private Long nextApprovalId;
+
+    /**
+     * 删除状态0未1已
+     */
+    private Integer delFlag;
+}
