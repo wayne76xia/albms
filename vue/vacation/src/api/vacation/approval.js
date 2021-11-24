7b5f308c7e3505daf49b048bfdb76cb4613eef54
bearcat926 <43287323+bearcat926@users.noreply.github.com>
2021-11-24 17:36:21 +0800

    Update approval.js

--- vue/vacation/src/api/vacation/approval.js
+++ vue/vacation/src/api/vacation/approval.js
@@ -1,10 +1,53 @@
 import request from '@/utils/request'
 
-// 假期列表
+
+// 假期审批列表
 export function holidayApprovalList(data) {
   return request({
     url: '/vacation/holiday/approval/list',
     method: 'GET',
     params: data
+  })
+}
+
+// 角色列表
+export function roleList() {
+  return request({
+    url: '/vacation/holiday/approval/roleList',
+    method: 'GET'
+  })
+}
+
+// 假期审批详细信息
+export function getHolidayApprovalInfo(id) {
+  return request({
+    url: '/vacation/holiday/approval/' + id,
+    method: 'GET'
+  })
+}
+
+// 新增假期审批
+export function holidayApprovalAdd(data) {
+  return request({
+    url: '/vacation/holiday/approval',
+    method: 'POST',
+    data: data
+  })
+}
+
+// 修改假期审批
+export function holidayApprovalUpdate(data) {
+  return request({
+    url: '/vacation/holiday/approval',
+    method: 'PUT',
+    data: data
+  })
+}
+
+// 删除假期审批
+export function holidayApprovalRemove(id) {
+  return request({
+    url: '/vacation/holiday/approval/' + id,
+    method: 'DELETE'
   })
 }
